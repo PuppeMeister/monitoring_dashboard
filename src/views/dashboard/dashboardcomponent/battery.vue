45397f4ef89958c26260025f0e1e11359952f2ae
Rin Nadia <luckylady9019@gmail.com>
2019-10-10 14:09:49 +0900

    State where there is no toogle button

--- src/views/dashboard/dashboardcomponent/battery.vue
+++ src/views/dashboard/dashboardcomponent/battery.vue
@@ -140,28 +140,7 @@
                         </div>
                        
                         <div class="col-md-4">
-                            <!-- <span class="badge badge-dark">on</span> -->
-                          <template>
-                                    <label :class="classList">
-                                        <input 
-                                            type="checkbox" 
-                                            class="switch-input"
-                                            :value="value"
-                                            :checked="isChecked"
-                                             @change="handleChange">
-
-                                                <template v-if="isOn">
-                                                        <span class="switch-label" :data-on="on" :data-off="off"></span>
-                                                </template>
-                                               
-                                                <template v-else>
-                                                        <span class="switch-label"></span>
-                                                </template>
-                                                
-                                                <span class="switch-handle"></span>
-                                    </label>
-                        </template>
-                         <!-- <span class="badge badge-dark">off</span> -->
+                           
                         </div>
                   </div>
 
@@ -183,24 +162,8 @@
     export default({
        
         name: 'battery',
-        model: {
-                prop: 'checked',
-                event: 'change'
-        },
-        props: ['title', 'power', 'current', 'voltage', 'level', 'charging', 'discharging',
-                {
-                    value: {default: true},
-                    uncheckedValue: { default: false},
-                    checked: {default: false},
-                    type: { type: String, default: 'default'},
-                    variant: {type: String, default: ''},
-                    pill: {type: Boolean,default: false},
-                    on: {type: String,default: null},
-                    off: {type: String,default: null},
-                    size: {type: String,default: null}
-                }
-        
-        ],
+       
+        props: ['title', 'power', 'current', 'voltage', 'level', 'charging', 'discharging'],
 
         methods: {
 
@@ -224,41 +187,12 @@
         
         },
 
-         computed: {
-                classList () {
-                return [
-                    'switch',
-                    this.switchType,
-                    this.switchVariant,
-                    this.switchPill,
-                    this.switchSize
-                    ]
-                },
-                switchType () {
-                return this.type ? `switch-${this.type}` : `switch-default`
-                },
-                switchVariant () {
-                return this.variant ? `switch-${this.variant}` : `switch-secondary`
-                },
-                switchPill () {
-                return !this.pill ? null : `switch-pill`
-                },
-                switchSize () {
-                return this.size ? `switch-${this.size}` : ''
-                },
-                isChecked () {
-                return this.checked === this.value
-                },
-                isOn () {
-                return !this.on ? null : true
-                }
-             },
-            methods: {
-                handleChange ({ target: { checked } }) {
-                // this.$emit('change', checked ? this.value : this.uncheckedValue)
-                e.target.parentElement.classList.toggle('close')
-                }
-            }
+        computed: {
+              
+        },
+        methods: {
+                
+        }
         
     })
 </script> 
